c8b564a04c514f20e1603971100a3f2b233d88d0
Avanish yadav <149554245+Avanishhyadav@users.noreply.github.com>
2024-11-02 23:46:42 +0530

    Delete apna_bank_App directory

--- apna_bank_App/src/main/java/test/DBInfo.java
+++
@@ -1,8 +0,0 @@
-package test;
-public interface DBInfo
-{
-public static final String
-dbURL="jdbc:oracle:thin:@localhost:1522:xe";
-public static final String dbUName="Avanish";
-public static final String dbPWord="vishal";
-}
